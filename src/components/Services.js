19428d4dec8b86e4dd7de066a239dbe0a9827ec5
Reginald Galang <reguv760@gmail.com>
2019-03-06 17:24:04 -0800

    services structured + bootstrap.js added to index.html

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,18 @@
-import React from 'react';
-
+import React from "react";
 
 const Services = () => {
-  return (
-    <div className="container">Services</div>
-  )
-}
+	return (
+		<div className="container">
+			<h1>Services</h1>
+			<ul className="list-group">
+				<li className="list-group-item">Cras justo odio</li>
+				<li className="list-group-item">Dapibus ac facilisis in</li>
+				<li className="list-group-item">Morbi leo risus</li>
+				<li className="list-group-item">Porta ac consectetur ac</li>
+				<li className="list-group-item">Vestibulum at eros</li>
+			</ul>
+		</div>
+	);
+};
 
 export default Services;
