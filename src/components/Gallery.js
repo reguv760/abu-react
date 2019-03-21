5bff6e3fb3aec436b39ca840d08f8f636c752672
Reginald Galang <reguv760@gmail.com>
2019-03-21 12:06:58 -0700

    more images added to gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,6 +7,10 @@ import ScrollableAnchor from "react-scrollable-anchor";
 import abuGarcetti from "./../assets/img/people/abuGarcetti.jpg";
 import abuCruzGarcetti from "./../assets/img/people/abuCruzGarcetti.jpg";
 import abuAnnCruzGarcetti from "./../assets/img/people/abuAnnCruzGarcetti.jpg";
+import abuAnnCruzGarcetti2 from "./../assets/img/people/abuAnnCruzGarcetti2.jpg";
+import abuAnnHontiveros from "./../assets/img/people/abuAnnHontiveros.jpg";
+import abuAnnRyu from "./../assets/img/people/abuAnnRyu.jpg";
+import abuAnnSotto from "./../assets/img/people/abuAnnSotto.jpg";
 import abu2 from "./../assets/img/people/abu2.jpg";
 import abuMary1 from "./../assets/img/people/abuMary1.jpg";
 import abuMary2 from "./../assets/img/people/abuMary2.jpg";
@@ -63,6 +67,77 @@ const Gallery = () => {
 									Philippine Consul General Adelio Ctuz(LL),
 									Annie Cuevas-Lim(L), LA Mayor Eric
 									Garcetti(M) and Abraham Lim(R).
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div className="col-md-6 col-lg-4">
+						<div className="card mb-4 box-shadow">
+							<img
+								alt="LA Mayor Eric Garcetti, Annie Cuevas-Lim,
+									Abraham Lim and Philippine Consul General
+									Adelio Ctuz."
+								className="card-img-top"
+								src={abuAnnCruzGarcetti2}
+							/>
+							<div className="card-body">
+								<p className="card-text">
+									LA Mayor Eric Garcetti, Annie Cuevas-Lim,
+									Abraham Lim and Philippine Consul General
+									Adelio Ctuz.
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div className="col-md-6 col-lg-4">
+						<div className="card mb-4 box-shadow">
+							<img
+								alt="Abraham Lim(L), Senator Risa Hontiveros(M) and
+									Annie Cuevas-Lim(R)."
+								className="card-img-top"
+								src={abuAnnHontiveros}
+							/>
+							<div className="card-body">
+								<p className="card-text">
+									Abraham Lim(L), Senator Risa Hontiveros(M)
+									and Annie Cuevas-Lim(R).
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div className="col-md-6 col-lg-4">
+						<div className="card mb-4 box-shadow">
+							<img
+								alt="Annie Cuevas-Lim(L), Los Angeles Councilmember David Ryu(M)
+									and Abraham Lim(L)."
+								className="card-img-top"
+								src={abuAnnRyu}
+							/>
+							<div className="card-body">
+								<p className="card-text">
+									Annie Cuevas-Lim(L), Los Angeles
+									Councilmember David Ryu(M) and Abraham
+									Lim(L).
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div className="col-md-6 col-lg-4">
+						<div className="card mb-4 box-shadow">
+							<img
+								alt="Annie Cuevas-Lim(L), Los Angeles Councilmember David Ryu(M)
+									and Abraham Lim(L)."
+								className="card-img-top"
+								src={abuAnnSotto}
+							/>
+							<div className="card-body">
+								<p className="card-text">
+									Senate President Tito Sotto(L), Annie
+									Cuevas-Lim(M) and Abraham Lim(L).
 								</p>
 							</div>
 						</div>
