1e0adc7f202d995724f61c0f9d8da324f4c83725
Reginald Galang <reguv760@gmail.com>
2019-03-21 22:31:24 -0700

    ilw scss created + mediaquery fixes

--- src/components/ILW.js
+++ src/components/ILW.js
@@ -2,19 +2,17 @@ import React from "react";
 
 const ILW = () => {
 	return (
-		<div className="ilw" style={{ margin: `1rem` }}>
+		<div className="ilw">
 			<div className="panel">
 				ILW Immigration Daily Updates
 				<br />
 				<iframe
+					className="ilw_frame"
 					src="https://www.ilw.com/parse.asp"
-					height="75"
-					width="100%"
 					scrolling="no"
 					frameBorder="0"
 					marginHeight="2"
 					marginWidth="2"
-					allowtransparency="true"
 					title="ILW"
 				/>
 			</div>
